aaa14f9f7b8f64b1d7aa33dfa8db52572ba933bb
fmontti <fmontti@gruposabra.com>
2021-05-15 17:22:59 -0300

    Cambios en slide

--- src/config/url.servicios.ts
+++ src/config/url.servicios.ts
@@ -7,11 +7,11 @@
 //export const URL_IMAGENES="http://192.168.1.7:8000/public/img/productos/";
 
 //laravel en local
-export const URL_SERVICIOS="http://127.0.0.1:8000";
-export const URL_IMAGENES="http://127.0.0.1:8000/public/img/productos/";
+// export const URL_SERVICIOS="http://127.0.0.1:8000";
+// export const URL_IMAGENES="http://127.0.0.1:8000/public/img/productos/";
 
 //https://almacen-api.herokuapp.com/
 
-// export const URL_SERVICIOS="http://134.209.123.95/";
-// export const URL_IMAGENES="http://134.209.123.95/public/img/productos/";
+export const URL_SERVICIOS="http://134.209.123.95/";
+export const URL_IMAGENES="http://134.209.123.95/public/img/productos/";
 
